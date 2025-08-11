9d92735e1b42863cd20f397696645ad9874c4cb8
aj <aj4ashutoshjha@gmail.com>
2025-08-11 16:20:32 +0530

    Switch to HashRouter for GitHub Pages routing

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
+import { HashRouter, Routes as RouterRoutes, Route } from "react-router-dom";
 import ScrollToTop from "components/ScrollToTop";
 import ErrorBoundary from "components/ErrorBoundary";
 import Header from "components/ui/Header";
 import NotFound from "pages/NotFound";
 import HomepageInteractiveDeveloperPortfolio from "pages/homepage-interactive-developer-portfolio";
 
-
 const Routes = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <ErrorBoundary>
         <ScrollToTop />
         <Header />
@@ -19,7 +18,7 @@ const Routes = () => {
           <Route path="*" element={<NotFound />} />
         </RouterRoutes>
       </ErrorBoundary>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
